d4a2e1ee3ac6fc1b7d76e7b10561b38fca44d78c
MihirGajjar27 <165854969+MihirGajjar27@users.noreply.github.com>
2025-02-08 17:44:21 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,10 @@
-// pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
-      <Head>
-        {/* Ensure proper scaling on mobile devices */}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <body>
+    <Html lang="en">
+      <Head />
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
